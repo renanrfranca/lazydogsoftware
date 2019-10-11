7a4074ea6268e15e1744fb10e6741bf9969bed05
Renan Rossignatti de França <renanrfranca@gmail.com>
2019-10-11 06:13:29 -0300

    últimos detalhes MVP 1

--- app/Http/Controllers/SimulationController.php
+++ app/Http/Controllers/SimulationController.php
@@ -11,14 +11,21 @@ class SimulationController extends Controller
     public function index() {
         $first_date = Serie_futuro::oldest('data')->first()->data;
         session(['next_date' => $first_date]);
+        session(['num_requests' => 0]);
         return view('dashboard');
     }
 
     public function getNewData() {
         $date = session('next_date');
+        $num_requests = session('num_requests');
+        session(['num_requests' => $num_requests + 1]);
 
         // Recupera do banco os dados da primeira data maior ou igual a requisitada
         $values = Serie_futuro::oldest('data')->where('data', '>=', $date->toDateString())->first();
+
+        if (!isset($values) || $num_requests > 10) {
+            return '{"status": "fim"}';
+        }
 
         // Pega data do valor retornado e increnta na sesão
         $date = Carbon::parse($values->data);
--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -6,14 +6,36 @@
             <div class="col-md-10">
                 <div class="card">
                     <div class="card-body">
+                        <div class="row">
+                            <div class="col">
+                                <h3 id="display-saldo"></h3>
+                            </div>
+                            <div class="col">
+                                <h3 id="display-preco"></h3>
+                            </div>
+                            <div class="col">
+                                <h3 id="display-estoque"></h3>
+                            </div>
+                        </div>
+
                         <div id="chart-candlestick">
 
                         </div>
 
-                        <div id="chart-bar">
-
+                        <div class="row">
+                            <div class="col">
+                                <div>
+                                    <input type="text" id="input-compra">
+                                    <button id="btn-compra">Comprar</button>
+                                </div>
+                            </div>
+                            <div class="col">
+                                <div>
+                                    <input type="text" id="input-venda">
+                                    <button id="btn-venda">Vender</button>
+                                </div>
+                            </div>
                         </div>
-                        <button id="btn-update" class="btn btn-primary">Atualizar</button>
                     </div>
                 </div>
             </div>
